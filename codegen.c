070c9424c6f7a5a640715f8d63afca15a8e7fb5c
Keita Morisaki <kmta1236@gmail.com>
2022-01-26 11:04:36 +0000

    refactor
    swtich -> if

--- codegen.c
+++ codegen.c
@@ -24,173 +24,187 @@ void PrintAssemblyForLeftVar(Node *node) {
 
 // Push processed result (VALUE) of node.
 void PrintAssembly(Node *node) {
-  // TODO(k1832): Replace switch-case with if.
-  // scope is too large when switch-case is used.
-  switch (node->kind) {
-    case ND_NUM:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      printf("  push %d\n", node->val);
-      return;
-    case ND_LVAR:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      PrintAssemblyForLeftVar(node);
-      printf("  pop rax\n");
-      printf("  mov rax, [rax]\n");
-      printf("  push rax\n");
-      return;
-    case ND_ASSIGN:
-      // Assign right to left and push right value to the stack.
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      PrintAssemblyForLeftVar(node->lhs);
-      PrintAssembly(node->rhs);
-      printf("  pop rdi\n");
-      printf("  pop rax\n");
-      printf("  mov [rax], rdi\n");
-      printf("  push rdi\n");
-      return;
-    case ND_RETURN:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      PrintAssembly(node->lhs);
-      printf("  pop rax\n");
-      printf("  mov rsp, rbp\n");
-      printf("  pop rbp\n");
-      // "ret" pops the address stored at the stack top, and jump there.
-      printf("  ret\n");
-      return;
-    case ND_IF:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      int label_for_else_statement = label_num++;
-      int label_for_if_end = label_num++;
+  if (node->kind == ND_NUM) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  if (node->kind == ND_LVAR) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    PrintAssemblyForLeftVar(node);
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+  }
+
+  if (node->kind == ND_ASSIGN) {
+    // Assign right to left and push right value to the stack.
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    PrintAssemblyForLeftVar(node->lhs);
+    PrintAssembly(node->rhs);
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+    printf("  mov [rax], rdi\n");
+    printf("  push rdi\n");
+    return;
+  }
+
+  if (node->kind == ND_RETURN) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    PrintAssembly(node->lhs);
+    printf("  pop rax\n");
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
+    // "ret" pops the address stored at the stack top, and jump there.
+    printf("  ret\n");
+    return;
+  }
+  if (node->kind == ND_IF) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    int label_for_else_statement = label_num++;
+    int label_for_if_end = label_num++;
+    PrintAssembly(node->condition);
+    printf("  pop rax\n");  // pop condition
+    printf("  cmp rax, 0\n");
+    // if condition is false, skip the if (body) statement
+    printf("  je .L%0*d\n", label_digit, label_for_else_statement);
+
+    PrintAssembly(node->body_statement);
+    printf("  jmp .L%0*d\n", label_digit, label_for_if_end);
+
+    printf(".L%0*d:\n", label_digit, label_for_else_statement);
+    if (node->else_statement) {
+      PrintAssembly(node->else_statement);
+    }
+
+    printf(".L%0*d:\n", label_digit, label_for_if_end);
+    return;
+  }
+
+  if (node->kind == ND_WHILE) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    int label_for_while_start = label_num++;
+    int label_for_while_end = label_num++;
+    printf(".L%0*d:\n", label_digit, label_for_while_start);
+    PrintAssembly(node->lhs);
+    printf("  pop rax\n");  // pop condition
+    printf("  cmp rax, 0\n");
+    // if condition is false, skip the while statement
+    printf("  je .L%0*d\n", label_digit, label_for_while_end);
+
+    PrintAssembly(node->rhs);
+    printf("  jmp .L%0*d\n", label_digit, label_for_while_start);
+
+    printf(".L%0*d:\n", label_digit, label_for_while_end);
+    return;
+  }
+
+  if (node->kind == ND_FOR) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    int label_for_for_start = label_num++;
+    int label_for_for_end = label_num++;
+    PrintAssembly(node->initialization);
+    printf(".L%0*d:\n", label_digit, label_for_for_start);
+    if (node->condition == NULL) {
+      printf("  push 1\n");  // HACK: condition is always true.
+    } else {
       PrintAssembly(node->condition);
-      printf("  pop rax\n");  // pop condition
-      printf("  cmp rax, 0\n");
-      // if condition is false, skip the if (body) statement
-      printf("  je .L%0*d\n", label_digit, label_for_else_statement);
+    }
+    printf("  pop rax\n");  // pop condition
+    printf("  cmp rax, 0\n");
+    // if condition is false, skip the for statement
+    printf("  je .L%0*d\n", label_digit, label_for_for_end);
 
-      PrintAssembly(node->body_statement);
-      printf("  jmp .L%0*d\n", label_digit, label_for_if_end);
+    PrintAssembly(node->body_statement);
+    PrintAssembly(node->iteration);
+    printf("  jmp .L%0*d\n", label_digit, label_for_for_start);
 
-      printf(".L%0*d:\n", label_digit, label_for_else_statement);
-      if (node->else_statement) {
-        PrintAssembly(node->else_statement);
-      }
+    printf(".L%0*d:\n", label_digit, label_for_for_end);
+    return;
+  }
 
-      printf(".L%0*d:\n", label_digit, label_for_if_end);
-      return;
-    case ND_WHILE:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      int label_for_while_start = label_num++;
-      int label_for_while_end = label_num++;
-      printf(".L%0*d:\n", label_digit, label_for_while_start);
-      PrintAssembly(node->lhs);
-      printf("  pop rax\n");  // pop condition
-      printf("  cmp rax, 0\n");
-      // if condition is false, skip the while statement
-      printf("  je .L%0*d\n", label_digit, label_for_while_end);
-
-      PrintAssembly(node->rhs);
-      printf("  jmp .L%0*d\n", label_digit, label_for_while_start);
-
-      printf(".L%0*d:\n", label_digit, label_for_while_end);
-      return;
-    case ND_FOR:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      int label_for_for_start = label_num++;
-      int label_for_for_end = label_num++;
-      PrintAssembly(node->initialization);
-      printf(".L%0*d:\n", label_digit, label_for_for_start);
-      if (node->condition == NULL) {
-        printf("  push 1\n");  // HACK: condition is always true.
-      } else {
-        PrintAssembly(node->condition);
-      }
-      printf("  pop rax\n");  // pop condition
-      printf("  cmp rax, 0\n");
-      // if condition is false, skip the for statement
-      printf("  je .L%0*d\n", label_digit, label_for_for_end);
-
-      PrintAssembly(node->body_statement);
-      PrintAssembly(node->iteration);
-      printf("  jmp .L%0*d\n", label_digit, label_for_for_start);
-
-      printf(".L%0*d:\n", label_digit, label_for_for_end);
-      return;
-    case ND_BLOCK:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+  if (node->kind == ND_BLOCK) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    node = node->next_in_block;
+    while (node) {
+      PrintAssembly(node);
+      printf("  pop rax\n");  // pop statement result
       node = node->next_in_block;
-      while (node) {
-        PrintAssembly(node);
-        printf("  pop rax\n");  // pop statement result
-        node = node->next_in_block;
-      }
-      return;
-    case ND_FUNC_CALL:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    }
+    return;
+  }
 
-      // Push arguments after the first 6 arguments
-      // reversely to stack
-      int argv_i = node->argc;
-      ArgsForCall *args_for_call = node->args_linked_list_head;
-      while (argv_i > 6) {
-        PrintAssembly(args_for_call->node);
-        // leave the result in stack
+  if (node->kind == ND_FUNC_CALL) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
 
-        args_for_call = args_for_call->next;
-        --argv_i;
-      }
-      while (argv_i) {
-        // Transfer results to registers specified by ABI.
-        PrintAssembly(args_for_call->node);
-        printf("  pop %s\n", registers[argv_i - 1]);
+    // Push arguments after the first 6 arguments
+    // reversely to stack
+    int argv_i = node->argc;
+    ArgsForCall *args_for_call = node->args_linked_list_head;
+    while (argv_i > 6) {
+      PrintAssembly(args_for_call->node);
+      // leave the result in stack
 
-        args_for_call = args_for_call->next;
-        --argv_i;
-      }
-      // TODO(k1832): 16byte allignment?
-      printf("  call %.*s\n", node->func_name_len, node->func_name);
-      printf("  push rax\n");
-      return;
-    case ND_FUNC_DECLARATION:
-      printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
-      printf("%.*s:\n", node->func_name_len, node->func_name);
-      // prologue
-      printf("  push rbp\n");
-      printf("  mov rbp, rsp\n");
-      const int num_variables = 26;
-      const int bytes_per_variable = 8;
-      printf("  sub rsp, %d\n", num_variables * bytes_per_variable);
+      args_for_call = args_for_call->next;
+      --argv_i;
+    }
+    while (argv_i) {
+      // Transfer results to registers specified by ABI.
+      PrintAssembly(args_for_call->node);
+      printf("  pop %s\n", registers[argv_i - 1]);
 
-      // Transfer argument values into stack frame
-      LVar *param = node->params_linked_list_head;
-      int param_i = node->num_parameters;
-      while (param_i > 6) {
-        // No need to transfer values for arguments after the first 6 arguments
-        // because they are allowed to be out of the stack frame and
-        // will be accessed with negative offset.
-        param = param->next;
-        --param_i;
-      }
+      args_for_call = args_for_call->next;
+      --argv_i;
+    }
+    // TODO(k1832): 16byte allignment?
+    printf("  call %.*s\n", node->func_name_len, node->func_name);
+    printf("  push rax\n");
+    return;
+  }
 
-      while (param_i) {
-        printf("  mov rax, rbp\n");
-        printf("  sub rax, %d\n", param->offset);
-        printf("  mov [rax], %s\n", registers[param_i - 1]);
-        param = param->next;
-        --param_i;
-      }
+  if (node->kind == ND_FUNC_DECLARATION) {
+    printf("  # %s (%s): at line %d\n", __FILE__, __func__, __LINE__);
+    printf("%.*s:\n", node->func_name_len, node->func_name);
+    // prologue
+    printf("  push rbp\n");
+    printf("  mov rbp, rsp\n");
+    const int num_variables = 26;
+    const int bytes_per_variable = 8;
+    printf("  sub rsp, %d\n", num_variables * bytes_per_variable);
 
+    // Transfer argument values into stack frame
+    LVar *param = node->params_linked_list_head;
+    int param_i = node->num_parameters;
+    while (param_i > 6) {
+      // No need to transfer values for arguments after the first 6 arguments
+      // because they are allowed to be out of the stack frame and
+      // will be accessed with negative offset.
+      param = param->next;
+      --param_i;
+    }
+
+    while (param_i) {
+      printf("  mov rax, rbp\n");
+      printf("  sub rax, %d\n", param->offset);
+      printf("  mov [rax], %s\n", registers[param_i - 1]);
+      param = param->next;
+      --param_i;
+    }
+
+    node = node->next_in_block;
+    while (node) {
+      PrintAssembly(node);
+      printf("  pop rax\n");
       node = node->next_in_block;
-      while (node) {
-        PrintAssembly(node);
-        printf("  pop rax\n");
-        node = node->next_in_block;
-      }
-      // epilogue
-      printf("  mov rsp, rbp\n");
-      printf("  pop rbp\n");
-      // "ret" pops the address stored at the stack top, and jump there.
-      printf("  ret\n");
-      return;
+    }
+    // epilogue
+    printf("  mov rsp, rbp\n");
+    printf("  pop rbp\n");
+    // "ret" pops the address stored at the stack top, and jump there.
+    printf("  ret\n");
+    return;
   }
 
   PrintAssembly(node->lhs);
